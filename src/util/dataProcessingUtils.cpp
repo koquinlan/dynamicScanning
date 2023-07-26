f05363cda5ca7def0fa3efdf076d3d81edf4af83
koquinlan <kyle.quinlan@colorado.edu>
2023-07-26 13:13:49 -0600

    Added small fix to phase unwrapping

--- src/util/dataProcessingUtils.cpp
+++ src/util/dataProcessingUtils.cpp
@@ -14,10 +14,10 @@
 void unwrapPhase(std::vector<double>& phase) {
     for (size_t i = 1; i < phase.size(); ++i) {
         double diff = phase[i] - phase[i - 1];
-        if (diff > M_PI) {
-            phase[i] -= 2 * M_PI;
-        } else if (diff < -M_PI) {
-            phase[i] += 2 * M_PI;
+        if (diff > 4*M_PI/5) {
+            phase[i] -= std::round(diff/M_PI) * M_PI;
+        } else if (diff < -4*M_PI/5) {
+            phase[i] += std::round(diff/M_PI) * M_PI;
         }
     }
 }
