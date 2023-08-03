c44debd2fb903818304397b82cc02f60ac805f27
koquinlan <kyle.quinlan@colorado.edu>
2023-08-03 11:28:52 -0600

    Fixed line endings

--- src/dataProcessing/MATLAB_baselining.m
+++ src/dataProcessing/MATLAB_baselining.m
@@ -12,7 +12,7 @@ Fs = 30e6; % sampling rate
 L = 15e4; % number of samples
 RBW = Fs/L;
 n_avg = 32;
-t_aq = L/Fs; 
+t_aq = L/Fs;
 digitsPrec = 7;
 f2Side = (-L/2:L/2-1)*(Fs/L)/1e6; % zero-centered frequency range
 
@@ -33,7 +33,7 @@ nSpectraBaseline = 10; % Number of spectra within each probe to use for baseline
 
 Noise_mat = zeros(nProbes, nSpectraBaseline,L);
 
-for i = 1:nProbes 
+for i = 1:nProbes
     filePattern = fullfile(FileList{i}, '*.mat');
     matFiles = dir(filePattern);
     
@@ -96,31 +96,31 @@ all_raw_data = cell(1, nProbes);
 
 for i = 1:nProbes
     raw_data = zeros(nProbes,L);
-
+    
     filePattern = fullfile(FileList{i}, '*.mat');
     matFiles = dir(filePattern);
-
+    
     for j = 1:nSpectra
         baseFileName = matFiles(j).name;
         FAxionData = load([FileList{i},'\',baseFileName],'PS2SideCC');
-        raw_data(j,:) = cell2mat(struct2cell(FAxionData(1))); 
-    end  
-
+        raw_data(j,:) = cell2mat(struct2cell(FAxionData(1)));
+    end
+    
     % ====================== remove the bad bins ======================
     % roughly remove the bad bins
     intermediate_spec_AllBins = raw_data./BaseLine_SG;
-
+    
     [foo, badbin_idx] = filloutliers(mean(intermediate_spec_AllBins), NaN, 'quartiles', ThresholdFactor=5);
-
+    
     raw_data(:,badbin_idx)= NaN;
-    raw_data = fillmissing(raw_data,'linear',2,EndValues='nearest'); % fill NaN value  
-
+    raw_data = fillmissing(raw_data,'linear',2,EndValues='nearest'); % fill NaN value
+    
     intermediate_spec_AllBins(:,badbin_idx)= NaN;
-    intermediate_spec_mat = fillmissing(intermediate_spec_AllBins,'linear',2,EndValues='nearest'); % fill NaN value  
+    intermediate_spec_mat = fillmissing(intermediate_spec_AllBins,'linear',2,EndValues='nearest'); % fill NaN value
     
     % Store the raw_data for each probe in the cell array
     all_raw_data{i} = raw_data;
-
+    
     csvFileName = sprintf('raw_data_probe_%d.csv', i);
     writematrix(raw_data, csvFileName);
 end
@@ -130,10 +130,10 @@ end
 
 % 	% ================ get processed spectra ==========
 %     Filter_SG = sgolayfilt(intermediate_spec_mat,SG_order,SG_framelen,[],2);
-%     processed_spec_mat = intermediate_spec_mat./Filter_SG-1;    
+%     processed_spec_mat = intermediate_spec_mat./Filter_SG-1;
 %     sigma_processed_expc= 1/sqrt(n_avg);
 %     sigma_processed_mat = sigma_processed_expc.*ones(nProbes,L+ShiftPoints*(nProbes-1));
-%         
+%
 %     % ================ shift the spectrum ==========
 %     rescaled_spec_mat = processed_spec_mat./vis;
 %     shifted_spec_mat =  nan(nProbes,L+ShiftPoints*(nProbes-1));
@@ -142,17 +142,17 @@ end
 %         shifted_spec_mat(i,(nProbes-i)*ShiftPoints+1:(nProbes-i)*ShiftPoints+L) = rescaled_spec_mat(i,:);
 %         shifted_vis_mat(i,(nProbes-i)*ShiftPoints+1:(nProbes-i)*ShiftPoints+L) = vis;
 %     end
-%     
+%
 %     % =========================== get combined spectrum =====================
 %     % get weight matrix
 %     sigma_rescaled_mat = sigma_processed_mat./shifted_vis_mat;
 %     sum_rows = nansum((1./sigma_rescaled_mat).^2);
 %     weight_mat = (1./sigma_rescaled_mat).^2./sum_rows; % Brubaker thesis eq 7.8 normalize the vis^2 mat in each column, has 0 in matrix element
 %     combined_spec(j,:) = nansum(weight_mat.*shifted_spec_mat); %  Brubaker thesis eq 7.9
-%     
+%
 %     % get std of combined spec
 %     sigma_combined_spec(j,:) = sum_rows.^(-1/2);
-%     
+%
 %     % get normalized combine spectrum
 %     norm_combined_spec(j,:) = combined_spec(j,:)./sigma_combined_spec(j,:); % normalized combined spec Brubaker thesis eq 7.11
 %     edges_norm_combined = linspace(min(norm_combined_spec),max(norm_combined_spec),nbins+1);
@@ -162,11 +162,11 @@ end
 %     fnc = fit(norm_combined_excess',norm_combined_counts','gauss1'); % f(x) =  a1*exp(-((x-b1)/c1)^2)
 %     sigma_norm_combined(j) = fnc.c1/sqrt(2);
 %     mean_norm_combined(j) = fnc.b1;
-%     
+%
 %     savedir = ['S:\lehnert\common\Quantum Metrology for Dark Matter\Joyce Jiang\measurement\',dates{1},'\DataProcessed'];
 %     save(fullfile(savedir,['\',CC_or_GC,'_FAxion',num2str(j)]),'ProbeInit','f2Side','BaseLine','BaseLine_SG','combined_spec','sigma_combined_spec',...
 %     'norm_combined_spec','sigma_norm_combined','mean_norm_combined')
 % end
-% 
-% 
-% 
+%
+%
+%
